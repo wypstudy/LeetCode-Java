a2f2be351ff6f2c0f9c50bc112f6aeef84223454
韦一平 <wei.yiping@frontsurf.com>
2020-06-30 10:23:10 +0800

    剑指 Offer 09. 用两个栈实现队列

---
+++ src/main/java/Offer_09.java
@@ -0,0 +1,33 @@
+import java.util.Stack;
+
+/**
+ * Date  :  2020/6/30
+ * Author:  YiPing, Wei
+ **/
+public class Offer_09 {
+    public static class CQueue {
+        Stack<Integer> a;
+        Stack<Integer> b;
+        public CQueue() {
+            a = new Stack<>();
+            b = new Stack<>();
+        }
+
+        public void appendTail(int value) {
+            while (!a.empty()) {
+                b.push(a.pop());
+            }
+            a.push(value);
+            while (!b.empty()) {
+                a.push(b.pop());
+            }
+        }
+
+        public int deleteHead() {
+            if (!a.empty())
+                return a.pop();
+            else
+                return -1;
+        }
+    }
+}
