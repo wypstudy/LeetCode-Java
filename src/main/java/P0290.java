86dc338c6cfded29d8c452bbd1a4d1c70b2fd9d8
wypstudy <wei.yiping@frontsurf.com>
2020-12-16 09:59:01 +0800

    290-单词规律 优化成只用一个Map

--- src/main/java/P0290.java
+++ src/main/java/P0290.java
@@ -4,24 +4,24 @@ import java.util.TreeMap;
 /**
  * Date  :  2020/12/16
  * Author:  YiPing, Wei
- * 简单题,两个Map存储字符查单词和单词查字符的字典即可
+ * 简单题,Map存储字符查单词的字典即可
  **/
 public class P0290 {
     public boolean wordPattern(String pattern, String s) {
         Map<Character, String> ch2word = new TreeMap<>();
-        Map<String, Character> word2ch = new TreeMap<>();
         String[] words = s.split(" ");
         if (words.length != pattern.length())
             return false;
         for (int i = 0; i < words.length; i++) {
             Character ch = pattern.charAt(i);
-            if (ch2word.containsKey(ch) || word2ch.containsKey(words[i])) {
-                if (!words[i].equals(ch2word.get(ch)) || !ch.equals(word2ch.get(words[i]))) {
+            if (ch2word.containsKey(ch)) {
+                if (!words[i].equals(ch2word.get(ch))) {
                     return false;
                 }
+            } else if (ch2word.containsValue(words[i])) {
+                return false;
             } else {
                 ch2word.put(ch, words[i]);
-                word2ch.put(words[i], ch);
             }
         }
         return true;
