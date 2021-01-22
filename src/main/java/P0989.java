bc64872d5ed3633d2d86dc2b755488712cf00096
wypstudy <1140664142@qq.com>
2021-01-22 20:18:19 +0800

    989-数组形式的整形加法 高精度

---
+++ src/main/java/P0989.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/*******************************************
+ * Author:    Wei.YiPing
+ * Date:      2021-01-22
+ *******************************************
+ **/
+public class P0989 {
+    public List<Integer> addToArrayForm(int[] A, int K) {
+        LinkedList<Integer> ans = new LinkedList<Integer>();
+        int ansLen = 10001;
+        int[] ansArr = new int[ansLen];
+        for (int i = 0; i < A.length; i++) {
+            ansArr[ansLen - A.length + i] = A[i];
+        }
+        int j = ansLen-1;
+        ansArr[j] += K;
+        while (j > 0 && ansArr[j] > 9) {
+            ansArr[j - 1] += ansArr[j] / 10;
+            ansArr[j--] %= 10;
+        }
+        j = 0;
+        while (j < ansLen && ansArr[j] == 0) j++;
+        for (int i = j; i < ansLen; i++) ans.add(ansArr[i]);
+        if (ans.size() == 0)
+            ans.add(0);
+        return ans;
+    }
+}
