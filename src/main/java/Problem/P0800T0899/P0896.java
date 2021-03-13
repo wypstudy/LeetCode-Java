2255b9c867195d8b60064201717fb0f0fae2efde
wypstudy <1140664142@qq.com>
2021-03-13 10:39:50 +0800

    896

---
+++ src/main/java/Problem/P0800T0899/P0896.java
@@ -0,0 +1,17 @@
+package Problem.P0800T0899;
+
+/*******************************************
+ * Author:    Wei.YiPing
+ * Date:      2021-02-28
+ *******************************************
+ **/
+public class P0896 {
+    public boolean isMonotonic(int[] A) {
+        boolean asc = true, dsc = true;
+        for (int i = 1; i < A.length; i++) {
+            if (A[i] < A[i - 1]) asc = false;
+            if (A[i] > A[i - 1]) dsc = false;
+        }
+        return asc || dsc;
+    }
+}
